b63cac6d5de257f1ad2c55467a0f7a3da7767e27
TypeErrorRic <ricardo.pabon@correounivalle.edu.co>
2023-09-17 20:42:50 -0500

    final

--- lib/ADC/ADC.h
+++ lib/ADC/ADC.h
@@ -2,26 +2,26 @@
 
 #define SIZE_TASK_ADC 1024
 
-//Parametros del ADC:
+// Parametros del ADC:
 typedef struct ADC_Parameters
 {
-    unsigned short usADC1;			    // Identificador del puerto del ADC 0.
-    unsigned short usADC2;			    // Identificador del puerto del ADC 1.
-    unsigned int uiADCRate;		        // La frecuencia de operación.
+    unsigned short usADC1;  // Identificador del puerto del ADC 0.
+    unsigned short usADC2;  // Identificador del puerto del ADC 1.
+    unsigned int uiADCRate; // La frecuencia de operación.
 } xADCParameters;
 
 /********** Definición de tareas **********/
 
-//Tarea 1 asociado al ADC 0:
+// Tarea 1 asociado al ADC 0:
 extern taskDefinition taskADC1;
-//Tarea 2 asociado al ADC 1:
+// Tarea 2 asociado al ADC 1:
 extern taskDefinition taskADC2;
 
 /**** Funciones para el funcionamiento ****/
 
-//Configuración del ADC:
+// Configuración del ADC:
 void setupTaskADCs();
-//Creación de la tarea para el ADC 1:
+// Creación de la tarea para el ADC 1:
 void ADCtask1(void *pvParameters);
-//Creación de la tarea para el ADC 2:
+// Creación de la tarea para el ADC 2:
 void ADCtask2(void *pvParameters);
