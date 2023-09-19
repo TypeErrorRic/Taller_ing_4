8784be9673ee95332dde374cc37ccbe13dd30d49
TypeErrorRic <ricardo.pabon@correounivalle.edu.co>
2023-09-19 18:44:18 -0500

    Guardar

--- src/modulosConfig.c
+++ src/modulosConfig.c
@@ -1,13 +1,14 @@
-#include "modulosConfig.h"
+#include "..\..\include\modulosConfig.h"
 
 //Lista de tareas a ejecutar:
-static taskDefinition * listTask[] = {&taskADC1, &taskADC2 /*Aquí se alistan las tareas*/};
+const taskDefinition * listTask[] = {&taskADC1, &taskADC2 /*Aquí se alistan las tareas*/};
 
 esp_err_t createTask()
 {
     static const char *TAG = "Create Task";
     if(sizeof(listTask) > 0)
     {
+        //Inicialización de tareas:
         for (short positionList = 0; positionList < (short)(sizeof(listTask)/sizeof(listTask[0])); positionList++)
         {
             xTaskCreate(listTask[positionList]->taskId, listTask[positionList]->name, listTask[positionList]->usStackDepth,
@@ -16,10 +17,12 @@ esp_err_t createTask()
         }
         return ESP_OK;
     }
+    //Retornanr error si no se manda a inicializar ninguna Tarea:
     ESP_LOGW(TAG, "No se ha asignado ninguna tarea.");
     return ESP_ERR_NOT_FOUND;
 }
 
+//Inicialización de variables: 
 esp_err_t initDrivers()
 {
     return ESP_OK;
